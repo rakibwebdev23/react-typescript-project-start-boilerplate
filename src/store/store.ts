f606ec0e4a845ac54c74cb18183b9424b160e969
rakibwebdev23 <rh.rakibhasan365@gmail.com>
2025-11-07 09:25:47 +0600

    redux setup all update

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,3 +1,4 @@
+// update code 
 import { configureStore } from '@reduxjs/toolkit'
 import {
   FLUSH,
@@ -14,14 +15,12 @@ import authReducer from './features/auth/auth.slice'
 import { baseAPI } from './api/baseApi'
 
 
-
 const persistConfig = {
   key: 'auth',
   storage
 }
 
 const persistedReducer = persistReducer(persistConfig, authReducer)
-
 
 export const store = configureStore({
   reducer: {
@@ -38,4 +37,50 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
-export const persistor = persistStore(store)
+export const persistor = persistStore(store);
+
+
+
+
+// previous code
+// import { configureStore } from '@reduxjs/toolkit'
+// import {
+//   FLUSH,
+//   PAUSE,
+//   PERSIST,
+//   persistReducer,
+//   persistStore,
+//   PURGE,
+//   REGISTER,
+//   REHYDRATE
+// } from 'redux-persist'
+// import storage from 'redux-persist/lib/storage'
+// import authReducer from './features/auth/auth.slice'
+// import { baseAPI } from './api/baseApi'
+
+
+
+// const persistConfig = {
+//   key: 'auth',
+//   storage
+// }
+
+// const persistedReducer = persistReducer(persistConfig, authReducer)
+
+
+// export const store = configureStore({
+//   reducer: {
+//     [baseAPI.reducerPath]: baseAPI.reducer,
+//     auth: persistedReducer,
+//   },
+//   middleware: getDefaultMiddleware =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+//       }
+//     }).concat(baseAPI.middleware)
+// })
+
+// export type RootState = ReturnType<typeof store.getState>
+// export type AppDispatch = typeof store.dispatch
+// export const persistor = persistStore(store)
